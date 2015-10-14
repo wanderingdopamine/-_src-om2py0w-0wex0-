34b1b2e73475b01da4646fbaf5dbf40b43cb9065
wanderingdopamine <shiiiiiiro@gmail.com>
2015-10-14 20:41:35 +0800

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbbed in."
+persian_cat = "I'm split\non a line."
+blackslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print blackslash_cat
+print fat_cat
+
+#extra
+
+test = "test"
+
+print "this is a %s" % test
+print "this is a %r" % test
+
